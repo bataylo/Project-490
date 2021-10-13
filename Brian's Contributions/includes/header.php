3c1f9d59c3003a92b2a878503850d12a42617f90
bataylo <78041089+bataylo@users.noreply.github.com>
2021-10-13 17:03:23 -0400

    Basic file directory.

---
+++ Brian's Contributions/includes/header.php
@@ -0,0 +1,29 @@
+<?php
+
+
+//header include file
+error_reporting(E_ALL);
+
+ini_set('display_errors', 1);
+
+$currentfile = basename($_SERVER['PHP_SELF']);
+
+require_once "includes/connect.php";
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Team 2 Card Collection</title>
+    <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=5o7mj88vhvtv3r2c5v5qo4htc088gcb51913qx5wlrtjn81y"></script>
+    <script>tinymce.init({ selector:'textarea' });</script>
+	 <link rel="stylesheet" href="includes/stylesheet.css">
+<header>
+    <nav>
+       
+    </nav>
+    <div class="centerh1"><h1 class="h1">Team 2's Card Collection Application</h1></div>
+</header>
